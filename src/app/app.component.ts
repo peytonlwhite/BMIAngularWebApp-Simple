26f4912564da5272e94286b6ea1faf4853356724
peyton white <peytonlwhite@yahoo.com>
2022-07-22 15:45:27 -0500

    bmi app done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'BMIWebApp';
+export class AppComponent implements OnInit {
+
+  title = 'BMIWebApp'
+  name:string = ''
+  height:number
+  weight:number
+  bmi:number
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  calcBMI() {
+    this.bmi = Math.round((this.weight / this.height / this.height) * 703)
+  }
+
+
+
+
+
 }
